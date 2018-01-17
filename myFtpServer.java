c38fc4451a38badc4796349c4f541a6ccbf6a8a7
mendkiakshay <akshay_mendki@hotmail.com>
2018-01-17 14:36:30 -0500

    mkdir with arguments.

--- myFtpServer.java
+++ myFtpServer.java
@@ -16,19 +16,26 @@ public static String mkdir(String folderName)
     return ex.toString();
   }
 }
+public static String[] splitCommand(String command)
+{
+  return command.split(" ");
+}
+
 public static void main(String[] args)
 {
 
 int portNumber = 9999;
-try{
-ServerSocket serSocket = new ServerSocket(9999);
-Socket socket = serSocket.accept();
-InputStreamReader reader = new InputStreamReader(socket.getInputStream());
-BufferedReader buffer = new BufferedReader(reader);
-String inputString = buffer.readLine();
-System.out.println(inputString);
+try
+{
+    ServerSocket serSocket = new ServerSocket(9999);
+    Socket socket = serSocket.accept();
+    InputStreamReader reader = new InputStreamReader(socket.getInputStream());
+    BufferedReader buffer = new BufferedReader(reader);
+    String inputString = buffer.readLine();
+    System.out.println(inputString);
 
-System.out.println(mkdir("newFolder1"));
+    if(splitCommand(inputString)[0].equals("mkdir"))
+        System.out.println(mkdir(splitCommand(inputString)[1]));
 
 }
 catch(Exception ex)
